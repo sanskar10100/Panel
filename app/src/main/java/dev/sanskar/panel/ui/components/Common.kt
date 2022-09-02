cea3788118bf2a44cc2dfe3b17d51e8f816a5cfd
Sanskar Agrawal <sanskar10100@gmail.com>
2022-09-02 18:01:00 +0530

    Add current questions UI in drawer

---
+++ app/src/main/java/dev/sanskar/panel/ui/components/Common.kt
@@ -0,0 +1,24 @@
+package dev.sanskar.panel.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+inline fun FullWithColumnWithCenteredChildren(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        content = content
+    )
+}
--- app/src/main/java/dev/sanskar/panel/ui/data/Question.kt
+++ app/src/main/java/dev/sanskar/panel/ui/data/Question.kt
@@ -1,8 +1,12 @@
 package dev.sanskar.panel.ui.data
 
+import dev.sanskar.panel.util.STRING_SEPARATOR
+
 data class Question(
     val questionText: String,
-    val type: Int, // enum class ordinal
+    val type: AnswerType, // enum class ordinal
     val correct: String, // multiple answers separated by =),
     val option: List<String> = mutableListOf()
 )
+
+fun String.getMultipleCorrectAnswers() = this.split(STRING_SEPARATOR)
