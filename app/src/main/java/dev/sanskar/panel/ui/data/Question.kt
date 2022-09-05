90d017699ffa73070666fee4ad0770fa28a67ad6
Sanskar Agrawal <sanskar10100@gmail.com>
2022-09-05 19:58:59 +0530

    Remove unnecessary data classes

--- app/src/main/java/dev/sanskar/panel/ui/data/Question.kt
+++ app/src/main/java/dev/sanskar/panel/ui/data/Question.kt
@@ -9,27 +9,4 @@ data class Question(
     val options: List<String> = mutableListOf()
 )
 
-data class FirebaseQuestion(
-    val questionText: String = "",
-    val type: Int,
-    val correct: String = "",
-)
-
-fun Question.toFirebaseQuestion() = FirebaseQuestion(
-    questionText = questionText,
-    type = type.ordinal,
-    correct = correct
-)
-
-fun FirebaseQuestion.toQuestion(options: List<String>) = Question(
-    questionText = questionText,
-    type = AnswerType.values()[type],
-    correct = correct,
-    options = options
-)
-
-data class FirebaseOption(
-    var optionText: String = ""
-)
-
 fun String.getMultipleCorrectAnswers() = this.split(STRING_SEPARATOR)
