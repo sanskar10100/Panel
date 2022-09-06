23d2758bca7dd2375e32770dce42ee94d8c05aa6
Sanskar Agrawal <sanskar10100@gmail.com>
2022-09-06 18:05:03 +0530

    Add answer state handling and display

--- app/src/main/java/dev/sanskar/panel/ui/components/Common.kt
+++ app/src/main/java/dev/sanskar/panel/ui/components/Common.kt
@@ -1,12 +1,19 @@
 package dev.sanskar.panel.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import dev.sanskar.panel.util.clickWithRipple
 
 @Composable
 inline fun FullWidthColumnWithCenteredChildren(
@@ -21,4 +28,28 @@ inline fun FullWidthColumnWithCenteredChildren(
         horizontalAlignment = horizontalAlignment,
         content = content
     )
+}
+
+@Composable
+fun InterceptClickBox(
+    modifier: Modifier = Modifier,
+    contentAlignment: Alignment = Alignment.TopStart,
+    childClickEnabled: Boolean = false,
+    onClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min),
+        contentAlignment = contentAlignment,
+    ) {
+        content()
+        if (!childClickEnabled) Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Transparent)
+                .clickWithRipple { onClick() }
+        )
+    }
 }
