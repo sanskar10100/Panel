03d7342bcdaa84d41f551fa248e47f27f95ebd8a
Sanskar Agrawal <sanskar10100@gmail.com>
2022-09-06 15:45:20 +0530

    Add question data fetch from Firebase

--- app/src/main/java/dev/sanskar/panel/ui/play/PlayQuizFragment.kt
+++ app/src/main/java/dev/sanskar/panel/ui/play/PlayQuizFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import dev.sanskar.panel.ui.components.ErrorDialog
+import dev.sanskar.panel.util.UiState
 
 class PlayQuizFragment : Fragment() {
     private val viewModel by viewModels<PlayViewModel>()
@@ -22,6 +29,8 @@ class PlayQuizFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel.loadQuiz(args.code)
+
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
@@ -32,6 +41,27 @@ class PlayQuizFragment : Fragment() {
 
     @Composable
     fun PlayQuizScreen(modifier: Modifier = Modifier) {
-        Text("Hello, your code is: ${args.code}")
+        when(val state = viewModel.quizQuestions) {
+            is UiState.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxSize(0.5f)
+                            .align(Alignment.Center)
+                    )
+                }
+            }
+            is UiState.Error -> {
+                ErrorDialog(message = state.message) { findNavController().navigateUp() }
+            }
+            is UiState.Success -> {
+                Text(
+                    modifier = Modifier.fillMaxSize(),
+                    text = state.data.toString()
+                )
+            }
+        }
     }
 }
